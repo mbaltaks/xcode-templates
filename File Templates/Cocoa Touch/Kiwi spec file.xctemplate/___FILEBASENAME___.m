2176afa30c0942091d71673ff54ea6caa5013a0d
Michael Baltaks <m@baltaks.com>
2013-10-25 17:43:17 +1100

    Add iOS test file templates.

---
+++ File Templates/Cocoa Touch/Kiwi spec file.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,27 @@
+//
+//  ___PROJECTNAME___ - ___FILENAME___
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Created by: ___FULLUSERNAME___
+//
+
+// Class under test
+#import "<#Class Header#>"
+
+// Collaborators
+
+// Test support
+#import "Kiwi.h"
+
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"___FILEBASENAMEASIDENTIFIER___", ^
+{
+    it(@"specs are not yet written", ^
+    {
+        [[theValue(YES) should] equal:theValue(NO)];
+    });
+});
+
+SPEC_END
---
+++ File Templates/Cocoa Touch/OCUnit test case (iOS).xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,34 @@
+//
+//  ___PROJECTNAME___ - ___FILENAME___
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Created by: ___FULLUSERNAME___
+//
+
+// Class under test
+#import "<#Class Header#>"
+
+// Collaborators
+
+// Test support
+#import <SenTestingKit/SenTestingKit.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+{
+    // test fixture ivars go here
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ___FILEBASENAMEASIDENTIFIER___");
+}
+
+@end
